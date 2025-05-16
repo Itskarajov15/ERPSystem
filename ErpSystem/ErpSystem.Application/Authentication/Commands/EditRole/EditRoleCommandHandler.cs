37eb91aa8c00ba4bb5cfac6d2a9eb42a29d7d856
Asen Karadzhov <asen__7@abv.bg>
2025-05-16 14:37:35 +0300

    Add command for editing roles.

--- ErpSystem/ErpSystem.Application/Authentication/Commands/EditRole/EditRoleCommandHandler.cs
+++ ErpSystem/ErpSystem.Application/Authentication/Commands/EditRole/EditRoleCommandHandler.cs
@@ -1,15 +1,64 @@
-﻿using ErpSystem.Domain.Entities.Identity;
+﻿using ErpSystem.Application.Common.Exceptions;
+using ErpSystem.Domain.Entities.Identity;
+using ErpSystem.Domain.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ErpSystem.Application.Authentication.Commands.EditRole;
 
 internal class EditRoleCommandHandler : IRequestHandler<EditRoleCommand>
 {
     private readonly RoleManager<ApplicationRole> _roleManager;
+    private readonly IRepository _repository;
 
-    public Task Handle(EditRoleCommand request, CancellationToken cancellationToken)
+    public EditRoleCommandHandler(RoleManager<ApplicationRole> roleManager, IRepository repository)
     {
-        throw new NotImplementedException();
+        _roleManager = roleManager;
+        _repository = repository;
+    }
+
+    public async Task Handle(EditRoleCommand request, CancellationToken cancellationToken)
+    {
+        var role = await _roleManager.FindByIdAsync(request.RoleId.ToString());
+
+        if (role == null)
+        {
+            throw new NotFoundException(nameof(ApplicationRole), request.RoleId);
+        }
+
+        if (
+            request.Dto.Name != role.Name
+            && await _roleManager.FindByNameAsync(request.Dto.Name) != null
+        )
+        {
+            throw new ArgumentException("Role with the same name already exists");
+        }
+
+        role.Name = request.Dto.Name;
+        role.NormalizedName = request.Dto.Name.ToUpper();
+        role.Description = request.Dto.Description;
+
+        var permissions = await _repository
+            .AllReadOnly<RoutePermission>()
+            .Where(rp => request.Dto.PermissionIds.Contains(rp.Id))
+            .ToListAsync();
+
+        var oldRolePermissions = await _repository
+            .AllReadOnly<RoleRoutePermission>()
+            .Where(rrp => rrp.RoleId == role.Id)
+            .ToListAsync();
+
+        _repository.DeleteRange(oldRolePermissions);
+
+        await _repository.AddRangeAsync(
+            permissions.Select(p => new RoleRoutePermission
+            {
+                RoleId = role.Id,
+                RoutePermissionId = p.Id,
+            })
+        );
+
+        await _repository.SaveChangesAsync();
     }
 }
